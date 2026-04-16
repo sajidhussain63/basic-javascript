ad241518a9e271ac664199766acaff49bcd231c0
Sajid Hussain <156522833+sajidhussain63@users.noreply.github.com>
2026-04-16 22:56:46 +0500

    Create day10.js
    Added day10.js

---
+++ 01_Basics/day10.js
@@ -0,0 +1,14 @@
+// Simple Object
+
+let person = {
+  name: "Sajid",
+  age: 20
+};
+
+// Accessing values
+
+// 1. Dot notation
+console.log(person.name);
+
+// 2. Bracket notation
+console.log(person["age"]);
